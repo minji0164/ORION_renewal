beb9f6a3e70d58ba6da2f8f4c33a3c6ffd596cbf
UX-KANGMINJI <ui@farmzonesnc.com>
2025-07-29 17:55:08 +0900

    메인페이지 작업

--- res/js/comm.js
+++ res/js/comm.js
@@ -1,14 +1,19 @@
+// 스크롤 라이브러리
 document.addEventListener('DOMContentLoaded', function() {
     const lenis = new Lenis({
         duration: 1,
         easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
         smooth: true,
     })
-    
     function raf(time) {
         lenis.raf(time)
         requestAnimationFrame(raf)
     }
-    
     requestAnimationFrame(raf)
 });
+
+// 슬라이드 라이브러리
+document.addEventListener( 'DOMContentLoaded', function() {
+    var splide = new Splide( '.splide' );
+    splide.mount();
+} );
